f615f8de2511572324dc794382de04477a406c47
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-31 20:36:49 +0000

    build out CRUD actions and restful routes in company_interests_controller

--- app/controllers/company_interests_controller.rb
+++ app/controllers/company_interests_controller.rb
@@ -3,9 +3,58 @@ class CompanyInterestsController < ApplicationController
   get '/company_interests' do 
     # can def error message in ApplicationController and call on that method here, which would then redirect user if not logged in
     
-    # Put above error message method here
+    # Put above redirect method (to error message) here
     @companies = CompanyInterest.all 
     erb :"/company_interests/index"
+  end 
+  
+  get "/company_interests/new" do 
+    # add redirect method (to error message) here 
+    # error_message = params[:error]
+    
+    erb :"/company_interests/new"
+  end 
+  
+  post "/company_interests" do 
+    ##  add redirect method (to error message) here 
+    # unless CompanyInterest.valid_params?(params)
+    #  redirect "/company_interests/new?error=invalid company_interest"
+    # end
+    
+    
+    # add redirect method (to error message) here 
+    CompanyInterest.create(params)
+    redirect to "/company_interests/#{@company_interest.id}"
+  end 
+  
+  get "/company_interests/:id/edit" do 
+    # add redirect method (to error message) here
+    # @error_message = params[:error]
+    
+    
+    @company = CompanyInterest.find(params[:id])
+     #if you have trouble with this one, or any others with find(params[:id]), try find_by_id(params[:id]) or look at Sinatra Complex Forms Associations for additional info. 
+     
+    erb :'/company_interests/edit'
+  end 
+  
+  get "/company_interests/:id" do 
+    # add redirect method (to error message) here 
+    
+    @company = CompanyInterest.find(params[:id])
+    erb :"/company_interests/show"
+  end 
+  
+  patch "/company_interests/:id" do 
+    # add redirect method (to error message) here 
+    
+    @company = CompanyInterest.find(params[:id])
+        # unless CompanyInterest.valid_params?(params)
+        #   redirect "/company_interests/#{@company.id}/edit?error=invalid company interest"
+        # end 
+    @company.update(params.select{|k| k == "name" || k == "description"})
+    redirect to "/company_interests/#{@company.id}"
+    # if you have trouble with this one, refer to Sinatra Complex Forms for additional options.
   end 
   
 end 
