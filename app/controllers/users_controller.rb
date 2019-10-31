b962ad9f42295526a527a60285b51d2a132e2dfa
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-31 17:09:22 +0000

    add views directories and files for company_interests and users, start building methods in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController 
   
+  get "/signup" do
+    # Will probably want to redirect them to the "users/new" if they're not a current user, or their personal index page ("/company_interests") if they are a current user. 
+    # /company_interests would then be defined in the company_interests controller and utilize erb :'/company_interests/index'
+    # Could then def error message in ApplicationController and call on that method at the beginning of this route in company_interests controller, which would then redirect user if not logged in
+    erb :signup
+  end
+
+  post "/signup" do
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.username != "" && user.save 
+      redirect '/login'
+    else 
+      # "/failure" below refers to views file for this example. (In Secure Password lab.)
+      # Probably best to show error messages another way. See "Video Review: Authentication" and golf example.
+      redirect '/failure' 
+    end
+  end
   
 end 
 
