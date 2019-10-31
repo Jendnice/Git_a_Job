8de57fb3aa533da635b051e4f4742c884c82c6a5
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-31 16:00:22 +0000

    update seed file naming

--- db/seed.rb
+++
@@ -1,6 +0,0 @@
-sophie = User.create(name: "Sophie", password: "sophie_password")
-flatiron = CompanyInterest.new(name: "Flatiron", description: "tech school/bootcamp")
-flatiron.user = sophie
-flatiron.save
-sophie.company_interests << flatiron 
-sophie.save 
